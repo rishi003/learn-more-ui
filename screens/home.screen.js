9276dc423f2092d5fb28270844e08ec3339957b6
Rishabh Awatani <rishabh.awatani@gmail.com>
2021-05-03 22:11:30 +0400

    Added icons and bottom bar

--- screens/home.screen.js
+++ screens/home.screen.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { useState } from "react";
 import { View, Text } from "react-native";
 import { StyleSheet } from "react-native";
-import { Button } from "react-native-paper";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Button, IconButton, Surface } from "react-native-paper";
+import Icon from "react-native-vector-icons/Ionicons";
+
 const Home = ({ navigation }) => {
+  const [defaultCollection, setDefaultCollection] = useState();
+
   return (
     <View style={style.container}>
       <Text>Home Screen</Text>
@@ -12,6 +18,15 @@ const Home = ({ navigation }) => {
       >
         Go to Collections
       </Button>
+      <Surface style={style.bottomBar}>
+        <TouchableOpacity>
+          <Icon name="documents-outline" size={25} color="black" />
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Icon name="duplicate-outline" size={25} color="black" />
+        </TouchableOpacity>
+      </Surface>
     </View>
   );
 };
@@ -21,6 +36,16 @@ const style = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  bottomBar: {
+    position: "absolute",
+    bottom: 0,
+    paddingVertical: 18,
+    width: "100%",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    flexDirection: "row",
   },
 });
 
